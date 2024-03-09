ff3788a1e1b5e167495a5c09f7c91df135be9ffa
Robert Guss <robertguss@gmail.com>
2024-03-09 15:02:17 -0500

    add DO url to next images in next config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,17 +4,21 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '1337',
-        pathname: '/uploads/**',
+        protocol: "http",
+        hostname: "localhost",
+        port: "1337",
+        pathname: "/uploads/**",
       },
       {
-        protocol: 'https',
-        hostname: 'images.pexels.com',
-      }
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+      {
+        protocol: "https",
+        hostname: "jellyfish-app-e6qcf.ondigitalocean.app",
+      },
     ],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
